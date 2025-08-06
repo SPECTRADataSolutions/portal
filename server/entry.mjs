7809592fa27e6b81b87605ce1e0b918135197737
MarkMaconnachie <MarkMaconnachie@users.noreply.github.com>
2025-08-06 23:11:33 +0000

    deploy: c9352514dd105c7a42857c25419c79b639babaa9

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C3_sFmq4.mjs';
-import { manifest } from './manifest_COlGsR4O.mjs';
+import { manifest } from './manifest_1iQE9FD3.mjs';
 
 const serverIslandMap = new Map();;
 
